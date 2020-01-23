b3651477e8ddf93a27045d6e4dc1107ccd222a52
Hudson Schier <hudsonschier@gmail.com>
2020-01-23 12:39:17 -0500

    Getting started on mapping

--- src/contest1.cpp
+++ src/contest1.cpp
@@ -3,12 +3,14 @@
 #include <geometry_msgs/Twist.h>
 #include <kobuki_msgs/BumperEvent.h>
 #include <sensor_msgs/LaserScan.h>
+#include <nav_msgs/GetMap.h>
 
 #include <stdio.h>
 #include <cmath>
 #include <cstdlib>
-
 #include <chrono>
+
+#define MAPPING_RATE 10000 // milliseconds
 
 // bumper
 #define LEFT 0
@@ -17,6 +19,12 @@
 // state
 #define RELEASED 0
 #define PRESSED 1
+
+struct laser_bumper_struct {
+    bool left;
+    bool center;
+    bool right;
+} laser_bumper;
 
 kobuki_msgs::BumperEvent bum;
 sensor_msgs::LaserScan laser;
@@ -27,35 +35,6 @@ void bumperCallback(const kobuki_msgs::BumperEvent msg)
 }
 
 void laserCallback(const sensor_msgs::LaserScan msg) {
-    // # Single scan from a planar laser range-finder
-    // #
-    // # If you have another ranging device with different behavior (e.g. a sonar
-    // # array), please find or create a different message, since applications
-    // # will make fairly laser-specific assumptions about this data
-
-    // Header header            # timestamp in the header is the acquisition time of 
-    //                          # the first ray in the scan.
-    //                          #
-    //                          # in frame frame_id, angles are measured around 
-    //                          # the positive Z axis (counterclockwise, if Z is up)
-    //                          # with zero angle being forward along the x axis
-                            
-    // float32 angle_min        # start angle of the scan [rad]
-    // float32 angle_max        # end angle of the scan [rad]
-    // float32 angle_increment  # angular distance between measurements [rad]
-
-    // float32 time_increment   # time between measurements [seconds] - if your scanner
-    //                          # is moving, this will be used in interpolating position
-    //                          # of 3d points
-    // float32 scan_time        # time between scans [seconds]
-
-    // float32 range_min        # minimum range value [m]
-    // float32 range_max        # maximum range value [m]
-
-    // float32[] ranges         # range data [m] (Note: values < range_min or > range_max should be discarded)
-    // float32[] intensities    # intensity data [device-specific units].  If your
-    //                          # device does not provide intensities, please leave
-    //                          # the array empty.
 	laser = msg;
 }
 
@@ -69,6 +48,8 @@ int main(int argc, char **argv)
 
     ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
 
+    ros::ServiceClient map_client = n.serviceClient<nav_msgs::GetMap>("gmapping/dynamic_map");
+
     ros::Rate loop_rate(10);
 
     geometry_msgs::Twist vel;
@@ -78,83 +59,25 @@ int main(int argc, char **argv)
     start = std::chrono::system_clock::now();
     uint64_t secondsElapsed = 0;
 
-    float angular = 0.0;
-    float linear = 0.1;
-    int state = 0;
-    float turntime;
-    float turndur = 3.5;
-    int randsign;
+    // update timer for mapping
+    std::chrono::time_point<std::chrono::system_clock> last_map_update;
+    last_map_update = std::chrono::system_clock::now();
 
     while(ros::ok() && secondsElapsed <= 480) {
         ros::spinOnce();
-        
-        if(bum.state == PRESSED) {
-            turntime = secondsElapsed;
-            linear = 0;
-            if(bum.bumper == RIGHT){
-                state = 1;
-            }
-            else if(bum.bumper == LEFT){
-                state = 2;
+        std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
 
-            }
-            else{
-                state = 3;
-                randsign = copysign(1, rand()%3-1);
-            }
+        // check if we should do mapping
+        if (std::chrono::duration_cast<std::chrono::milliseconds>(now - last).count() > MAPPING_RATE) {
+            
+            
+
+            now = std::chrono::system_clock::now();
+            last_map_update = now;
         }
 
-        if(state == 0){
-            linear = 0.1;
-            angular = 0.0;
-        }
-
-        if(state == 1){
-            if(secondsElapsed - turntime < 0.5){
-                linear = -0.1;
-            }
-            else if(secondsElapsed - turntime > 0.5){
-                linear = 0;
-                angular = 0.5;
-            }
-            if(secondsElapsed - turntime > turndur){
-                state = 0;
-            }
-        }
-
-        if(state == 2){
-            if(secondsElapsed - turntime < 0.5){
-                linear = -0.1;
-            }
-            else if(secondsElapsed - turntime > 0.5){
-                linear = 0;
-                angular = -0.5;
-            }
-            if(secondsElapsed - turntime > turndur){
-                state = 0;
-            }
-        }
-
-        if(state == 3){
-            if(secondsElapsed - turntime < 0.5){
-                linear = -0.1;
-            }
-            else if(secondsElapsed - turntime > 0.5){
-                linear = 0;
-                angular = randsign*0.5;
-            }
-            if(secondsElapsed - turntime > turndur){
-                state = 0;
-            }
-        }
-        
-        
-
-
-
-
-        vel.angular.z = angular;
-        vel.linear.x = linear;
+        vel.angular.z; // angular
+        vel.linear.x; // linear
         vel_pub.publish(vel);
 
         // The last thing to do is to update the timer.
