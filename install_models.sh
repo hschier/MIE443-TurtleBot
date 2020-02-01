02910377d7ab960c9beeace27435800da2133ce0
Hudson Schier <hudsonschier@gmail.com>
2020-01-31 19:49:31 -0500

    got sim working

---
+++ install_models.sh
@@ -0,0 +1,2 @@
+sudo mkdir /usr/share/gazebo/models
+sudo cp models/* /usr/share/gazebo/models
---
+++ run_sim.sh
@@ -0,0 +1,3 @@
+export TURTLEBOT_GAZEBO_WORLD_FILE=/home/hudson/catkin_ws/src/MIE443-TurtleBot/worlds/playground.world
+export GAZEBO_MODEL_PATH=/home/hudson/catkin_ws/src/MIE443-TurtleBot/worlds:$GAZEBO_MODEL_PATH
+roslaunch turtlebot_gazebo turtlebot_world.launch
